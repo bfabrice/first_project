8adc08418528a70f2a3f92f17e756fa92a279e0d
bfabrice <bafabros@yahoo.fr>
2014-11-19 11:19:11 +0200

    this is my first flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+__author__ = 'john'
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h2>Hello, World!</h2>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
