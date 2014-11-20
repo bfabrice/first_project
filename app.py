9f84a2e0f7add6d39eaf33599b86c4d92bc2dd2f
bfabrice <bafabros@yahoo.fr>
2014-11-20 11:59:24 +0200

    my flask app with response

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 __author__ = 'john'
 
 
-from flask import Flask
+from flask import Flask,make_response
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ def index():
     return "<h2>Hello, World!</h2>"
 @app.route('/greeting/<name>')
 def greet(name):
-    return '<h1> Hello, %s! amakuru to murugo </h1>'%name
+    return make_response('<h1> Hello, %s! amakuru to murugo </h1>'%name)
 
 
 @app.route('/counter/<int:number>')
@@ -18,13 +19,18 @@ def greet(name):
 def counter(number):
     numbers = ('zero','one','two','three','four','five')
     if number <= 5:
-        return '<h1> number %s!</h1>'%numbers[number]
+        return make_response('<h1> number %s!</h1>'%numbers[number])
     else:
-        return '<h1> the number is not in da range </h1>'
+        return make_response('<h1> the number is not in da range </h1>')
 @app.route('/count/<int:value>')
 def count(value):
     values = {1:'one',2:'two'}
-    return "Number %s"% values.get(value,'the number is no in the range')
+    return make_response("Number %s"% values.get(value,'the number is no in the range'))
+
+@app.route('/browser')
+def req():
+    user_agent = request.headers.get('User-Agent')
+    return make_response('<p> Your browser is %s </p>'%user_agent)
 
 
 if __name__ == '__main__':
